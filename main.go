aeb5586bfb29b731b32ec5ab7cd3d94c6ea032c9
Albin George <albinin0002@gmail.com>
2019-01-27 23:33:44 +0530

    Fix document printer errors

--- main.go
+++ main.go
@@ -21,8 +21,9 @@ func main() {
 	paymentID := os.Args[1]
 
 	results := sumocred.Search(paymentID + " (GATEWAY_SOAP_REQUEST or GATEWAY_REQUEST_TIMEOUT or PAYMENT_CALLBACK_REQUEST or GATEWAY_PAYMENT_REQUEST or GATEWAY_CHECKSUM_VERIFY_FAILED or GATEWAY_ERROR_DATA_MISMATCH or GATEWAY_ERROR_TIMED_OUT or GATEWAY_ERROR_USER_INACTIVE or GATEWAY_ERROR_HASH_GENERATION_ERROR)  | json auto nodrop")
-	//formatter.Format(results)
+
 	formattedResult := formatter.Format(results)
 
+	printer.PrintStdout(formattedResult)
 	printer.PrintDoc(formattedResult)
 }
--- printer/doc_printer.go
+++ printer/doc_printer.go
@@ -1,6 +1,7 @@
 package printer
 
 import "baliance.com/gooxml/document"
+import "github.com/albingeorge/sumofetch/formatter"
 
 func PrintDoc(input []formatter.FormattedContent) {
 	doc := document.New()

e44f717d5aa68bc57cb96059c12e1be83f803528
Albin George <albinin0002@gmail.com>
2019-01-29 16:18:19 +0530

    Add screenshots
    You can pass an absolute path of a directory in which the screenshots
    are present as the 3rd argument

--- printer/doc_printer.go
+++ printer/doc_printer.go
@@ -1,9 +1,13 @@
 package printer
 
 import (
+	"baliance.com/gooxml/common"
 	"baliance.com/gooxml/document"
+	"baliance.com/gooxml/measurement"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 import "github.com/albingeorge/sumofetch/formatter"
@@ -11,6 +15,7 @@ import "github.com/albingeorge/sumofetch/formatter"
 func PrintDoc(input []formatter.FormattedContent) {
 
 	testPath, testName := "", ""
+
 	if len(os.Args) > 2 {
 		testPath = os.Args[2]
 		splittedString := strings.Split(testPath, "/")
@@ -26,7 +31,9 @@ func PrintDoc(input []formatter.FormattedContent) {
 
 	addText(doc, testName, "", true)
 
-	addText(doc, "Screenshots", "", false)
+	//addText(doc, "Screenshots", "", false)
+
+	addScreenshots(doc)
 
 	for _, content := range input {
 		addText(doc, content.Header, "", true)
@@ -65,4 +72,67 @@ func addText(doc *document.Document, content string, style string, bold bool) {
 		run.AddBreak()
 	}
 
+}
+
+func addScreenshots(doc *document.Document) {
+
+	if len(os.Args) > 3 {
+
+		root := os.Args[3]
+
+		imageReferences := []common.ImageRef{}
+
+		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+
+			// If it's a directory, skip it
+			if info.IsDir() {
+				return nil
+			}
+
+			img, err := common.ImageFromFile(path)
+			if err != nil {
+				fmt.Println("Unable to create image from ", path, ". Error: ", err)
+				return nil
+			}
+
+			iref, err := doc.AddImage(img)
+
+			if err != nil {
+				fmt.Println("Error adding image: ", err)
+				return nil
+			}
+
+			imageReferences = append(imageReferences, iref)
+
+			return nil
+		})
+
+		if err != nil {
+			return
+		}
+
+		para := doc.AddParagraph()
+		run := para.AddRun()
+		run.AddText("Screenshots")
+		run.AddBreak()
+
+		for _, iref := range imageReferences {
+			//_, err := run.AddDrawingAnchored(iref)
+			inl, err := run.AddDrawingInline(iref)
+
+			iref.RelID()
+
+			if err != nil {
+				log.Fatalf("unable to add inline image: %s", err)
+			}
+
+			inl.SetSize(5.5*measurement.Inch, 3.5*measurement.Inch)
+
+			run.AddBreak()
+			run.AddBreak()
+		}
+
+	} else {
+		fmt.Println("Skipping adding screenshots, since no path provided")
+	}
 }
